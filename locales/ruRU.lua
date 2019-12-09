f09bcb016175084ab284998f4cd8fc85f6a239c8
Rubgrsch <rubgrsch@gmail.com>
2019-12-09 09:11:21 +0800

    Add Russian translation
    Contributed by Hubbotu

---
+++ locales/ruRU.lua
@@ -0,0 +1,13 @@
+local _, dt = ...
+local _, L = unpack(dt)
+
+if GetLocale() ~= "ruRU" then return end
+
+L["enableTargetFrame"] = "Включить Окно Цели"
+L["enableTargetFrameTooltips"] = "Показать время смерти для цели"
+L["font"] = "Шрифт"
+L["fontSize"] = "Размер Шрифта"
+L["mover"] = "Двигать"
+L["moverTooltip"] = "Нажмите, чтобы начать перемещение."
+L["moverMsg"] = "Перетащите влево для перемещения окон; Щелкните ПКМ, чтобы заблокировать их."
+L["DTFrame"] = "Окно DT"
