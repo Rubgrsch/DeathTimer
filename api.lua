d99b421367caa95602c6b515a0ddf0405c62c5a2
Rubgrsch <rubgrsch@gmail.com>
2019-12-24 12:22:02 +0800

    Remove api.lua
    Add api to wiki soon

--- api.lua
+++
@@ -1,7 +0,0 @@
--- APIs for other addons
---
-DeathTimer.GetDeathTime([unit])
---
--- Return estimate death time (in seconds) for given unit (target if not given).
--- Return nil if estimate time <= 0 or data not available.
---
